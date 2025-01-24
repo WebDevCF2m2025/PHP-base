fa960338422b26289d27a2d2afa6ed056549a2d2
agim <agim.coroli8016@gmail.com>
2025-01-24 12:43:31 +0100

    fin exo 12

--- stagiaires/agim/12-GET.php
+++ stagiaires/agim/12-GET.php
@@ -54,15 +54,56 @@ $webdev_2025 = [
     <a href="12-GET.php?classe=WEBDEV03">Farah Said</a><br>
     <a href="12-GET.php?classe=WEBDEV04">Horban Mykyta</a><br>
     <br>
+
+    
+    <?php $eleve1 = [$webdev_2025["WEBDEV01"]["Nom"], $webdev_2025["WEBDEV01"]["Prenom"]]?>
+    <?php $sitePersoEleve1 = [$webdev_2025["WEBDEV01"]["Sites"]["SitePerso"]]?>
+    <?php $sitePrefoEleve1 = [$webdev_2025["WEBDEV01"]["Sites"]["SitePreformation"]]?>
+    <?php $githubEleve1 = [$webdev_2025["WEBDEV01"]["Sites"]["github"]]?>
+
+    <?php $eleve2 = [$webdev_2025["WEBDEV02"]["Nom"], $webdev_2025["WEBDEV02"]["Prenom"]]?>
+    <?php $sitePersoEleve2 = [$webdev_2025["WEBDEV02"]["Sites"]["SitePerso"]]?>
+    <?php $sitePrefoEleve2 = [$webdev_2025["WEBDEV02"]["Sites"]["SitePreformation"]]?>
+    <?php $githubEleve2 = [$webdev_2025["WEBDEV02"]["Sites"]["github"]]?>
+
+    <?php $eleve3 = [$webdev_2025["WEBDEV03"]["Nom"], $webdev_2025["WEBDEV03"]["Prenom"]]?>
+    <?php $sitePersoEleve3 = [$webdev_2025["WEBDEV03"]["Sites"]["SitePerso"]]?>
+    <?php $sitePrefoEleve3 = [$webdev_2025["WEBDEV03"]["Sites"]["SitePreformation"]]?>
+    <?php $githubEleve3 = [$webdev_2025["WEBDEV03"]["Sites"]["github"]]?>
+
+    <?php $eleve4 = [$webdev_2025["WEBDEV04"]["Nom"], $webdev_2025["WEBDEV04"]["Prenom"]]?>
+    <?php $sitePersoEleve4 = [$webdev_2025["WEBDEV04"]["Sites"]["SitePerso"]]?>
+    <?php $sitePrefoEleve4 = [$webdev_2025["WEBDEV04"]["Sites"]["SitePreformation"]]?>
+    <?php $githubEleve4 = [$webdev_2025["WEBDEV04"]["Sites"]["github"]]?>
+
     <?php if (isset($_GET['classe']) && $_GET['classe'] === 'WEBDEV01'): ?>
-        <?php $firstStudent = $webdev_2025['WEBDEV01']['Nom'] . " " . $webdev_2025['WEBDEV01']['Prenom'] ?>
-        <h1><?php echo $firstStudent ?><br></h1>
-        <h2>Mes sites : <br></h2>
-        <?php $websites = $webdev_2025['WEBDEV01']['Sites'] ?>
-        <?php foreach ($websites as $key => $website): ?>
-            <p> <?php echo $key . " : " ?> <a href="<?php echo $website ?>"><?php echo $website ?></a></p>
-        <?php endforeach ?>
-    <?php endif ?>
-</body>
+        <h1><?php echo $eleve1[0] . " " . $eleve1[1] ?></h1>
+        <h2>Site perso :  <a href="<?php echo $sitePersoEleve1[0] ?>"><?php echo $sitePersoEleve1[0] ?></a> </h2>
+        <h2>Site prefo :  <a href="<?php echo $sitePrefoEleve1[0] ?>"><?php echo $sitePrefoEleve1[0] ?></a> </h2>
+        <h2>Github :  <a href="<?php echo $githubEleve1[0] ?>"><?php echo $githubEleve1[0] ?></a> </h2>
+
+        <?php elseif (isset($_GET['classe']) && $_GET['classe'] === 'WEBDEV02'): ?>
+        <h1><?php echo $eleve2[0] . " " . $eleve2[1] ?></h1>
+        <h2>Site perso :  <a href="<?php echo $sitePersoEleve2[0] ?>"><?php echo $sitePersoEleve2[0] ?></a> </h2>
+        <h2>Site prefo :  <a href="<?php echo $sitePrefoEleve2[0] ?>"><?php echo $sitePrefoEleve2[0] ?></a> </h2>
+        <h2>Github :  <a href="<?php echo $githubEleve2[0] ?>"><?php echo $githubEleve2[0] ?></a> </h2>
+
+        <?php elseif (isset($_GET['classe']) && $_GET['classe'] === 'WEBDEV03'): ?>
+        <h1><?php echo $eleve3[0] . " " . $eleve3[1] ?></h1>
+        <h2>Site perso :  <a href="<?php echo $sitePersoEleve3[0] ?>"><?php echo $sitePersoEleve3[0] ?></a> </h2>
+        <h2>Site prefo :  <a href="<?php echo $sitePrefoEleve3[0] ?>"><?php echo $sitePrefoEleve3[0] ?></a> </h2>
+        <h2>Github :  <a href="<?php echo $githubEleve3[0] ?>"><?php echo $githubEleve3[0] ?></a> </h2>
+        
+        <?php elseif (isset($_GET['classe']) && $_GET['classe'] === 'WEBDEV04'): ?>
+        <h1><?php echo $eleve4[0] . " " . $eleve4[1] ?></h1>
+        <h2>Site perso :  <a href="<?php echo $sitePersoEleve4[0] ?>"><?php echo $sitePersoEleve4[0] ?></a> </h2>
+        <h2>Site prefo :  <a href="<?php echo $sitePrefoEleve4[0] ?>"><?php echo $sitePrefoEleve4[0] ?></a> </h2>
+        <h2>Github :  <a href="<?php echo $githubEleve4[0] ?>"><?php echo $githubEleve4[0] ?></a> </h2>
+        <?php endif ?>
+    </body>
 
 </html>
+
+
+
+

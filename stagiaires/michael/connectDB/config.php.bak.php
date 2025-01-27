9457861881e2dc6cf736568922906b17ffd71728
WebDevCF2m <gitweb@cf2m.be>
2025-01-27 16:05:48 +0100

    prepare DB

---
+++ stagiaires/michael/connectDB/config.php.bak.php
@@ -0,0 +1,10 @@
+<?php
+
+// constantes de la base de donnée
+const DB_MYSQL_TYPE = "mysql";
+const DB_MYSQL_HOST = ""; // hôte de connexion (url ou adresse ip)
+const DB_MYSQL_NAME = ""; // nom de la DB
+const DB_MYSQL_LOGIN = ""; // login
+const DB_MYSQL_PWD = ""; // mot de passe
+const DB_MYSQL_PORT = 3306;
+const DB_MYSQL_ENCODE = "utf8";
---
+++ stagiaires/michael/connectDB/index.php
@@ -0,0 +1,3 @@
+<?php
+# redirecttion
+header("Location: public/");
